ed9acfde0f1085ea031dd7cb708f0eaec73e3202
WhoisJohnKim <johnkim9417@gmail.com>
2018-08-09 21:36:49 -0500

    Finished functionality of the homework. Styling is still very basic.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-// Initialize Firebase
+ // Initialize Firebase
 var config = {
     apiKey: "AIzaSyDxdjcWApq-UvYA3qTWG4QODBkKYhbfJoo",
     authDomain: "traintime-8de64.firebaseapp.com",
@@ -26,13 +26,49 @@ function nextTrain(start, frequency){
     return moment(next).format("hh:mm")
 }
 
+// Find the time to the next train
+function timeToNextTrain(start, frequency){
+    var firstTimeConverted = moment(start, "HH:mm").subtract(1, "years");
+    var currentTime = moment();
+    var timeDifference = moment().diff(moment(firstTimeConverted), "minutes");
+    var remainder = timeDifference % frequency;
+    var minutesToNextTrain = frequency - remainder;
+    console.log(minutesToNextTrain);
+    return minutesToNextTrain;
+}
+
+// Every time the page is loaded or a new entry is entered, change HTML to reflect the database
+database.ref().on("child_added", function(childSnapshot) {
+    console.log(childSnapshot.val());
+    var newRow = $("<tr>");
+    var nameEntry = $("<td>").text(childSnapshot.val().name);
+    var destinationEntry = $("<td>").text(childSnapshot.val().destination);
+    var frequencyEntry = $("<td>").text(childSnapshot.val().frequency);
+    var nextTrainEntry = $("<td>").text(nextTrain(childSnapshot.val().startTime, childSnapshot.val().frequency));
+    var remainderEntry = $("<td>").text(timeToNextTrain(childSnapshot.val().startTime, childSnapshot.val().frequency));
+    newRow.append(nameEntry);
+    newRow.append(destinationEntry);
+    newRow.append(frequencyEntry);
+    newRow.append(nextTrainEntry);
+    newRow.append(remainderEntry);
+
+    $("#trainSchedule").prepend(newRow);
+
+});
+
 $("#submitButton").on("click", function(){
     // Make sure that all input fields are filled
     if(freqInput.val() === "" || nameInput.val() === "" || destInput.val() === "" || timeInput.val() === ""){
         alert("Please fill out all inputs before adding a new train. Thank you!");
     }
     else{
-        var nextTrainTime = nextTrain(timeInput.val(),freqInput.val());
-        console.log(nextTrainTime);
+        // Push entered values to the database on firebase
+        database.ref().push({
+            name: nameInput.val().trim(),
+            destination: destInput.val().trim(),
+            startTime: timeInput.val().trim(),
+            frequency: freqInput.val().trim()
+        })
+
     }
 })
